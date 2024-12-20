ad598238c3a7f022e67c0d5cf01af1d55c87a20b
milyasbpa <basilyas.2015@gmail.com>
2024-12-21 01:09:44 +0700

    fix: add scroll bar indicator

--- src/core/modules/app/fragments/navigation/Navigation.app.tsx
+++ src/core/modules/app/fragments/navigation/Navigation.app.tsx
@@ -16,6 +16,26 @@ export const NavigationApp = () => {
     }
   }, [darkMode]);
 
+  const [scrollProgress, setScrollProgress] = React.useState<number>(0);
+
+  const handleScroll = () => {
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const progress = (scrollTop / scrollHeight) * 100;
+    setScrollProgress(progress);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleClickThemeMode = () => {
     dispatch({
       type: AppActionEnum.SetThemeData,
@@ -28,33 +48,41 @@ export const NavigationApp = () => {
   return (
     <nav
       className={clsx(
-        "flex items-center justify-center",
+        "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
         "fixed",
         "top-0",
         "z-[20]",
         "w-full",
-        "h-[60px]",
+        "opacity-80",
         "px-[1.5rem] desktop:px-0",
+        "pt-[1rem]",
         "bg-white dark:bg-[#171717]"
       )}
     >
-      <div
-        className={clsx("flex items-center justify-end", "w-full max-w-6xl")}
-      >
-        <button
-          className={clsx(
-            "w-[2rem] h-[2rem]",
-            "flex items-center justify-center",
-            "rounded-[0.5rem]",
-            darkMode ? "text-white" : "text-dark25",
-            "border border-grey80 dark:border-dark18"
-          )}
-          aria-label="theme"
-          onClick={handleClickThemeMode}
+      <div className={clsx("flex items-center justify-center", "w-full")}>
+        <div
+          className={clsx("flex items-center justify-end", "w-full max-w-6xl")}
         >
-          {darkMode ? <FaMoon size={16} /> : <FaSun size={16} />}
-        </button>
+          <button
+            className={clsx(
+              "w-[2rem] h-[2rem]",
+              "flex items-center justify-center",
+              "rounded-[0.5rem]",
+              darkMode ? "text-white" : "text-dark25",
+              "border border-grey80 dark:border-dark18"
+            )}
+            aria-label="theme"
+            onClick={handleClickThemeMode}
+          >
+            {darkMode ? <FaMoon size={16} /> : <FaSun size={16} />}
+          </button>
+        </div>
       </div>
+
+      <div
+        className={clsx("bg-purple78", "h-[2px]")}
+        style={{ width: `${scrollProgress}%` }}
+      />
     </nav>
   );
 };
