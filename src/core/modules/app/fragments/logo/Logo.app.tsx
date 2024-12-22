374c38699dee1101488c2892facfd2249e3a6541
milyasbpa <basilyas.2015@gmail.com>
2024-12-22 18:23:41 +0700

    fix: scroller link

--- src/core/modules/app/fragments/logo/Logo.app.tsx
+++ src/core/modules/app/fragments/logo/Logo.app.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import Image from "next/image";
 import clsx from "clsx";
+import Link from "next/link";
+import { getDictionaries } from "../../i18n";
 
 export const LogoApp = () => {
+  const dictionaries = getDictionaries();
   return (
-    <Image
-      src={"/logo/favicon-96x96.png"}
-      alt="logo"
-      width={32}
-      height={32}
-      className={clsx("rounded-[0.5rem]")}
-    />
+    <Link href={dictionaries.navigation.logo.href}>
+      <Image
+        src={dictionaries.navigation.logo.src}
+        alt={dictionaries.navigation.logo.alt}
+        width={32}
+        height={32}
+        className={clsx("rounded-[0.5rem]")}
+      />
+    </Link>
   );
 };
