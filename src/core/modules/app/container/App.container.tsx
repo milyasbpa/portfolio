883eb8ae526dfb70f21dab35ea95e28c93c39afd
milyasbpa <basilyas.2015@gmail.com>
2024-12-22 17:15:04 +0700

    fix: refactor menu navbar

--- src/core/modules/app/container/App.container.tsx
+++ src/core/modules/app/container/App.container.tsx
@@ -1,8 +1,9 @@
 "use client";
 import * as React from "react";
 import clsx from "clsx";
-import { NavigationApp } from "../fragments/navigation";
-import { AppProvider } from "../context";
+import { MenuApp } from "../fragments/menu";
+import { ScrollProgressApp } from "../fragments/scroll_progress";
+import { ThemeApp } from "../fragments/theme";
 
 export interface AppContainer {
   children?: React.ReactNode;
@@ -16,7 +17,39 @@ export const AppContainer = ({ children }: AppContainer) => {
         "w-full"
       )}
     >
-      <NavigationApp />
+      <nav
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
+          "fixed",
+          "top-0",
+          "z-[20]",
+          "w-full",
+          "opacity-80",
+          "px-[1.5rem] desktop:px-0",
+          "pt-[1rem]",
+          "bg-white dark:bg-[#171717]"
+        )}
+      >
+        <div className={clsx("flex items-center justify-center", "w-full")}>
+          <div
+            className={clsx(
+              "flex items-center justify-end lg:justify-between",
+              "w-full max-w-6xl",
+              "px-[1rem]"
+            )}
+          >
+            {/* menu */}
+            <MenuApp />
+            {/* settings */}
+            <div className={clsx("flex items-center justify-end gap-[1rem]")}>
+              {/* theme */}
+              <ThemeApp />
+            </div>
+          </div>
+        </div>
+
+        <ScrollProgressApp />
+      </nav>
       {children}
     </main>
   );
